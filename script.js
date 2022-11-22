3e81a07b89c54b86d8af040762eb1a0d62c005aa
KTNamgay <ktnamgay18@gmail.com>
2022-11-22 12:39:42 +0600

    Riddle1

---
+++ script.js
@@ -0,0 +1,13 @@
+var cards = document.querySelectorAll(".card");
+cards.forEach (function(card){
+    card.addEventListener("click", function() {
+       
+        if (card.classList.contains("open")) {
+            card.className = "card";
+           
+        }
+        else {
+            card.className = "card open";
+        }
+    });
+});
